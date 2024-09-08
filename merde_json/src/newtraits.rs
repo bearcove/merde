105a734e552701a68c2446b9735833c2484aeb1a
Amos Wenger <amos@bearcove.net>
2024-09-08 17:37:41 +0200

    hello breakthrough

--- merde_json/src/newtraits.rs
+++ merde_json/src/newtraits.rs
@@ -1,4 +1,9 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, collections::HashMap, sync::Arc};
+
+use jiter::{JsonObject, JsonValue, LazyIndexMap};
+use smallvec::SmallVec;
+
+use crate::JsonValueExt;
 
 pub enum JsonValue2<'src> {
     Str(Cow<'src, str>),
@@ -19,6 +24,88 @@ where
 {
     t
 }
+
+fn covariant_jsonvalue_clone<'longer, 'shorter>(
+    t: crate::JsonValue<'longer>,
+) -> crate::JsonValue<'shorter>
+where
+    'longer: 'shorter,
+{
+    match t {
+        JsonValue::Null => JsonValue::Null,
+        JsonValue::Bool(b) => JsonValue::Bool(b),
+        JsonValue::Int(i) => JsonValue::Int(i),
+        JsonValue::BigInt(bi) => JsonValue::BigInt(bi),
+        JsonValue::Float(f) => JsonValue::Float(f),
+        JsonValue::Str(s) => JsonValue::Str(s),
+        JsonValue::Array(a) => {
+            let mut new_a: SmallVec<[JsonValue<'_>; 8]> = Default::default();
+            for v in a.iter() {
+                new_a.push(covariant_jsonvalue_clone(v.clone()));
+            }
+            JsonValue::Array(Arc::new(new_a))
+        }
+        JsonValue::Object(o) => {
+            let mut new_o: LazyIndexMap<Cow<'_, str>, JsonValue<'_>> = Default::default();
+            for (k, v) in o.iter() {
+                new_o.insert(k.clone(), covariant_jsonvalue_clone(v.clone()));
+            }
+            JsonValue::Object(Arc::new(new_o))
+        }
+    }
+}
+
+fn covariant_jsonobject<'longer, 'shorter>(
+    t: crate::JsonObject<'longer>,
+) -> crate::JsonObject<'shorter>
+where
+    'longer: 'shorter,
+{
+    t
+}
+
+fn covariant_jsonarray<'longer, 'shorter>(
+    t: crate::JsonArray<'longer>,
+) -> crate::JsonArray<'shorter>
+where
+    'longer: 'shorter,
+{
+    t
+}
+
+fn covariant_lazyindexmap<'longer, 'shorter, K, V>(
+    t: jiter::LazyIndexMap<Cow<'longer, str>, String>,
+) -> jiter::LazyIndexMap<Cow<'shorter, str>, String>
+where
+    'longer: 'shorter,
+{
+    t
+}
+
+fn covariant_smallvec<'longer, 'shorter, T>(
+    t: SmallVec<[Cow<'longer, str>; 8]>,
+) -> SmallVec<[Cow<'shorter, str>; 8]>
+where
+    'longer: 'shorter,
+{
+    t
+}
+
+fn covariant_smallvec_maybe<'longer, 'shorter, T>(
+    t: SmallVec<[Cow<'longer, str>; 8]>,
+) -> SmallVec<[Cow<'shorter, str>; 8]>
+where
+    'longer: 'shorter,
+{
+    t.into_iter()
+        .map(|s| match s {
+            Cow::Borrowed(s) => Cow::Borrowed(s),
+            Cow::Owned(s) => Cow::Owned(s.clone()),
+        })
+        .collect()
+}
+
+// -------------------------------------------------------------------------
 
 pub trait JsonDeserialize2<'src>
 where
@@ -45,7 +132,7 @@ impl<'src> JsonDeserialize2<'src> for Cow<'src, str> {
     }
 }
 
-///
+// -------------------------------------------------------------------------
 
 pub trait JsonDeserialize3<'src>
 where
@@ -56,13 +143,21 @@ where
     ) -> Result<Self, crate::MerdeJsonError>;
 }
 
-impl<'src> JsonDeserialize3<'src> for Cow<'src, str> {
+impl<'src> JsonDeserialize3<'src> for HashMap<Cow<'src, str>, JsonValue<'src>> {
     fn json_deserialize<'val>(
         value: Option<&'val crate::JsonValue<'src>>,
     ) -> Result<Self, crate::MerdeJsonError> {
         let value = value.ok_or(crate::MerdeJsonError::MissingValue)?;
         match value {
-            crate::JsonValue::Str(s) => Ok(s.clone()),
+            crate::JsonValue::Object(s) => {
+                let mut map = HashMap::new();
+                for (key, val) in s.iter() {
+                    let parsed_key = key.clone();
+                    let parsed_value = val.clone();
+                    map.insert(parsed_key, parsed_value);
+                }
+                Ok(map)
+            }
             _ => Err(crate::MerdeJsonError::MismatchedType {
                 expected: crate::JsonFieldType::String,
                 // not true
