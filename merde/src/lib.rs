8e7acef2b42886fc70d5c6b89677fc33c5a1215f
Amos Wenger <amos@bearcove.net>
2024-09-09 21:12:03 +0200

    Introduce our own value type

---
+++ merde/src/lib.rs
@@ -0,0 +1,15 @@
+mod cowstr;
+mod lazyindexmap;
+
+pub use cowstr::CowStr;
+pub use lazyindexmap::LazyIndexMap;
+
+pub enum Value<'s> {
+    Int(i64),
+    Float(f64),
+    String(CowStr<'s>),
+    Null,
+    Bool(bool),
+    Array(Vec<Value<'s>>),
+    Object(LazyIndexMap<CowStr<'s>, Value<'s>>),
+}
